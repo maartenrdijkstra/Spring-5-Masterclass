0281dda2c51eef92db6574765c47840f36b4d912
Maarten Dijkstra <maartenrdijkstra@gmail.com>
2020-09-15 10:07:56 +0200

    Add Services, Controllers, Models and JSP files

---
+++ todolist/src/main/java/academy/learnprogramming/service/TodoItemServiceImpl.java
@@ -0,0 +1,35 @@
+package academy.learnprogramming.service;
+
+import academy.learnprogramming.model.TodoData;
+import academy.learnprogramming.model.TodoItem;
+import lombok.Getter;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoItemServiceImpl implements TodoItemService{
+
+    // == fields ==
+    @Getter
+    private final TodoData data = new TodoData();
+
+    @Override
+    public void addItem(TodoItem todoItem) {
+        data.addItem(todoItem);
+    }
+
+    @Override
+    public void removeItem(int id) {
+        data.removeItem(id);
+    }
+
+    @Override
+    public TodoItem getItem(int id) {
+        return data.getItem(id);
+    }
+
+    @Override
+    public void updateItem(TodoItem todoItem) {
+        data.updateItem(todoItem);
+    }
+
+}
